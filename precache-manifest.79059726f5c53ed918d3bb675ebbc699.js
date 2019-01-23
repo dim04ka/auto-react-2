0176f1613ebc45b49274301d4cf80a00864950ff
Dmitry Suhotsky <dmitry.suhotsky@gmail.com>
2019-01-23 11:20:59 +0300

    Deploy

---
+++ precache-manifest.79059726f5c53ed918d3bb675ebbc699.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "cb820aefef80a7720c06",
+    "url": "/static/css/main.001019bd.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "608cd639068dcf0edbe5",
+    "url": "/static/js/1.608cd639.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cb820aefef80a7720c06",
+    "url": "/static/js/main.cb820aef.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "608cd639068dcf0edbe5",
+    "url": "/static/css/1.a344f764.chunk.css"
+  },
+  {
+    "revision": "d61916e338651332dfa2afe49f4ede66",
+    "url": "/index.html"
+  }
+];
